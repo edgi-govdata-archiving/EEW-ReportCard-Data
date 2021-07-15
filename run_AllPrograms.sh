fc9e04426d873ebbcf9fa0894737ae33c99ccf34
shansen5 <steven.e.hansen@gmail.com>
2021-07-14 21:56:09 -0700

    Back up current region.db before re-populating.

--- run_AllPrograms.sh
+++ run_AllPrograms.sh
@@ -1,3 +1,16 @@
+#!/bin/bash
+
+CURRENTDATE=`date +"%Y-%m-%d"`
+SAVENAME=region.db-${CURRENTDATE}
+echo 'Saving the current region.db'
+cp region.db ${SAVENAME}
+echo 'Saving the log file from the last population of region.db'
+SAVENAME=AllPrograms.log-${CURRENTDATE}
+
+echo 'Cleaning out the affected tables'
+sqlite3 region.db < clean_regions.sql
+
+echo 'Running the AllPrograms.py commands to populate region.db'
 python AllPrograms.py -c state_cd-1.csv -f 2020 > AllPrograms.log
 python AllPrograms.py -c state_cd-2.csv -f 2020 >> AllPrograms.log
 python AllPrograms.py -c state_cd-3.csv -f 2020 >> AllPrograms.log
