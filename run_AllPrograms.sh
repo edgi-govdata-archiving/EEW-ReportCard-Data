147fe01670f7a0d83bf7cd0602d8b86f80c72529
shansen5 <steven.e.hansen@gmail.com>
2021-08-02 18:52:33 +0000

    Add separate error logging.

--- run_AllPrograms.sh
+++ run_AllPrograms.sh
@@ -5,9 +5,11 @@ cd /home/steve/EEW-ReportCard-Data/
 CURRENTDATE=`date +"%Y-%m-%d"`
 SAVENAME=region.db-${CURRENTDATE}
 LOGNAME=AllPrograms.log-${CURRENTDATE}
+ERRORNAME=AllPrograms.error-${CURRENTDATE}
 mv AllPrograms.log ${LOGNAME}
+mv AllPrograms.error ${ERRORNAME}
 
-echo 'Saving the current region.db' > AllPrograms.log
+echo 'Saving the current region.db' > AllPrograms.log 2> AllPrograms.error
 cp region.db ${SAVENAME}
 date >> AllPrograms.log
 echo 'Saving the log file from the last population of region.db' >> AllPrograms.log
@@ -17,21 +19,21 @@ sqlite3 region.db < clean_regions.sql
 
 echo 'Running the AllPrograms.py commands to populate region.db' >> AllPrograms.log
 date >> AllPrograms.log
-python3 AllPrograms.py -c state_cd-1.csv -f 2020 >> AllPrograms.log
+python3 AllPrograms.py -c state_cd-1.csv -f 2020 >> AllPrograms.log 2>> AllPrograms.error
 date >> AllPrograms.log
-python3 AllPrograms.py -c state_cd-2.csv -f 2020 >> AllPrograms.log
+python3 AllPrograms.py -c state_cd-2.csv -f 2020 >> AllPrograms.log 2>> AllPrograms.error
 date >> AllPrograms.log
-python3 AllPrograms.py -c state_cd-3.csv -f 2020 >> AllPrograms.log
+python3 AllPrograms.py -c state_cd-3.csv -f 2020 >> AllPrograms.log 2>> AllPrograms.error
 date >> AllPrograms.log
-python3 AllPrograms.py -c state_cd-4.csv -f 2020 >> AllPrograms.log
+python3 AllPrograms.py -c state_cd-4.csv -f 2020 >> AllPrograms.log 2>> AllPrograms.error
 date >> AllPrograms.log
-python3 AllPrograms.py -c state_cd-5.csv -f 2020 >> AllPrograms.log
+python3 AllPrograms.py -c state_cd-5.csv -f 2020 >> AllPrograms.log 2>> AllPrograms.error
 date >> AllPrograms.log
-python3 AllPrograms.py -c state_cd-6.csv -f 2020 >> AllPrograms.log
+python3 AllPrograms.py -c state_cd-6.csv -f 2020 >> AllPrograms.log 2>> AllPrograms.error
 date >> AllPrograms.log
-python3 AllPrograms.py -c state_cd-7.csv -f 2020 >> AllPrograms.log
+python3 AllPrograms.py -c state_cd-7.csv -f 2020 >> AllPrograms.log 2>> AllPrograms.error
 date >> AllPrograms.log
-python3 AllPrograms.py -c state_cd-8.csv -f 2020 >> AllPrograms.log
+python3 AllPrograms.py -c state_cd-8.csv -f 2020 >> AllPrograms.log 2>> AllPrograms.error
 date >> AllPrograms.log
-python3 AllPrograms.py -c state_cd-9.csv -f 2020 >> AllPrograms.log
+python3 AllPrograms.py -c state_cd-9.csv -f 2020 >> AllPrograms.log 2>> AllPrograms.error
 date >> AllPrograms.log
