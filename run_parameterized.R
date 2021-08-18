e946f32c7957ca236c9c41725ccef65e8e8495d8
shansen5 <steven.e.hansen@gmail.com>
2021-08-18 16:46:09 -0700

    Updated run_parameterized.R

--- run_parameterized.R
+++ run_parameterized.R
@@ -1,6 +1,7 @@
 library(pagedown)
 library(here)
 library(purrr)
+library(dplyr)
 
 load("names.rda")
 names <- cd2
@@ -8,17 +9,21 @@ rm(cd2)
 
 param_table <- names %>%
   rename(
+    region = Region,
     cd_state = Identity,
-    state = State
-  ) %>% select(cd_state,state,full_name)
+    state = State,
+    district = District
+  ) %>% select(cd_state,state,district,full_name)
 
 target <- c("TX34", "AL01")
 #so for shiny we would want the target to be specific2
 selected <- param_table %>% filter(param_table$cd_state %in% target)
 
-render_report <- function(cd_state = selected$cd_state,
+render_report <- function(region = selected$region,
+                          cd_state = selected$cd_state,
                           full_name = selected$full_name,
-                          state = selected$state) {
+                          state = selected$state,
+                          district = selected$district) {
 
   template <- "CD_template.rmd"
 
@@ -26,20 +31,21 @@ render_report <- function(cd_state = selected$cd_state,
 
   parameters <- list(cd_state = cd_state,
                      full_name=full_name,
-                     state=state)
+                     state=state,
+                     district=district)
 
   pagedown::chrome_print(rmarkdown::render(template, params = parameters,
-  envir = new.env(),clean = TRUE, output_dir = ("Output"), output_file = out_file,
+    envir = new.env(),clean = TRUE, output_dir = ("Output"), output_file = out_file,
   ))
 
  # invisible(TRUE)
 }
 
 #use this function if you just want to run one report
-render_report()
+# render_report()
 
 
-render_report(cd_state = selected$cd_state, full_name = selected$full_name, state = selected$state)
+# render_report(cd_state = selected$cd_state, full_name = selected$full_name, state = selected$state)
 
 #use these two if you want to run multiple reports
 params_list <- as.list(selected)
