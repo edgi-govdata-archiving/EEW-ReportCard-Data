d8dc12a8b988999d704d282d596102869258ccf6
shansen5 <steven.e.hansen@gmail.com>
2021-12-01 19:22:23 +0000

    Fix full name parameter

--- run_reportcards.sh
+++ run_reportcards.sh
@@ -3,9 +3,9 @@
 source .profile
 cd ${EEW_HOME}
 
-Rscript run_state_reportcards.R -s '^[A-I]'
-Rscript run_state_reportcards.R -s '^[J-R]'
-Rscript run_state_reportcards.R -s '^[S-Z]'
 Rscript run_CD_reportcards.R -s '^[A-I]'
 Rscript run_CD_reportcards.R -s '^[J-R]'
 Rscript run_CD_reportcards.R -s '^[S-Z]'
+Rscript run_state_reportcards.R -s '^[A-I]'
+Rscript run_state_reportcards.R -s '^[J-R]'
+Rscript run_state_reportcards.R -s '^[S-Z]'
