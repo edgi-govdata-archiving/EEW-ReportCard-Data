9a5cf07fc5cd286902d708775015a488036cf6fa
msc249 <97808539+msc249@users.noreply.github.com>
2022-10-26 11:53:32 -0400

    added run_all_districts

--- Phase_1/CD_report_gen.py
+++ Phase_1/CD_report_gen.py
@@ -1,2 +1,14 @@
-#hello
-print("hello")
+import numpy
+import pandas
+
+def run_all_districts(state):
+    """
+    Loops through all of the congressional districts of a state and calls gen_report(state, cd) on them
+
+    Parameter state: str of desired state to examine
+    """
+    
+    data = pandas.read_table('state'+'.txt', sep='|',header =None,names=['BLOCKID','DISTRICT'])
+    dists = data['DISTRICT'].unique()
+    for cd in dists:
+        gen_report(state, cd)
