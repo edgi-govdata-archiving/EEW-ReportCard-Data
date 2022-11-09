47de76aafe82a44f80a7ce176d057c5349a93a84
msc249 <97808539+msc249@users.noreply.github.com>
2022-11-08 22:48:45 -0500

    added cloning function

--- Phase_1/CD_report_gen.py
+++ Phase_1/CD_report_gen.py
@@ -3,6 +3,7 @@ import folium
 import pandas as pd
 from ECHO_modules.get_data import get_echo_data
 import sqlite3
+from git import Repo
 
 
 def cd_to_block(state, cd):
@@ -117,3 +118,8 @@ def gen_report(state, cd):
             except AttributeError:
                 continue
     return newdf
+
+def clone_repo():
+    Repo.clone_from("https://github.com/edgi-govdata-archiving/ECHO_modules", "edgi-govdata-archiving/ECHO_modules")
+    
+    
