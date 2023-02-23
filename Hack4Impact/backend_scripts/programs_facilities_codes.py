b88c67b52ce0b9ef387129b4c6b44f15e3c15398
Max Wang <maxwang@Maxs-MacBook-Pro-2.local>
2023-02-23 17:06:08 -0500

    for luke

--- Hack4Impact/backend_scripts/programs_facilities_codes.py
+++ Hack4Impact/backend_scripts/programs_facilities_codes.py
@@ -1,1 +1,22 @@
-#hi
+# Make selections about programs, active facilities, and NAICS codes
+import ipywidgets 
+def make_selections():
+    programs = ipywidgets.SelectMultiple(
+        options=['NPDES_FLAG', 'AIR_FLAG', 'RCRA_FLAG'],
+        description='Programs',
+        disabled=False
+    )
+    active = ipywidgets.Checkbox(
+        value=False,
+        description='Only active facilities?',
+        disabled=False
+    )
+    naics = ipywidgets.Text(
+        value='',
+        placeholder='Enter NAICS codes here, separated with a comma', #2111,213111,213112
+        description='NAICS codes',
+        disabled=False
+    )
+    display(programs, active, naics)
+
+
